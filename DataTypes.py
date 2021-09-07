5076e861e8cb68735c8347261e79b9599e89c620
John Snow <e14036122@gmail.com>
2021-09-08 01:01:59 +0800

    Add new file

---
+++ DataTypes.py
@@ -0,0 +1,17 @@
+'''data types
+資料類型
+1.整數 integer int
+2.福點數 float
+3.布林值 boolean bool (True,False)
+4.字串 string str(文字)
+'''
+
+name = 'John'
+rain = True
+pen = 0.366
+
+print(name, rain, pen)
+
+name = input('請輸入名字: ')
+
+print('嗨',name)
